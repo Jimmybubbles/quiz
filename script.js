98656ae052a8efd5ce519d9fa5c3e725b3c2307c
james <james.anthony.russell36@gmail.com>
2022-09-04 19:17:45 +1000

    made timer count down to 0 and have divs to populate

--- script.js
+++ script.js
@@ -8,17 +8,16 @@ const countdownEL = document.getElementById('countdown');
 t = setInterval(updateCountdown, 1000);
 
 
+
 function updateCountdown(){
     const minutes = Math.floor(time / 60);
     let seconds = time % 60;
-
     seconds = seconds < 10 ? '0' + seconds : seconds;
-
     countdownEL.innerHTML = `${minutes}:${seconds}`;
 
     time--;
 
-    if(time === 0){
+    if(time === -1){
         clearInterval(t);
     }
     
