f37e3233a630a42dbe0a30b6e25819767f50b885
james <james.anthony.russell36@gmail.com>
2022-09-04 14:50:12 +1000

    made clock in seperate html to understand format

---
+++ questions.js
@@ -0,0 +1,29 @@
+var questions [
+    
+    {
+        question: "What operator returns the remainder left over when one operand is divided by a second operand.",
+        multiChoice: ["+=", "&", "%", "#"],
+        answer: "% Modulo"
+    },
+
+    {
+        question: "What does .length do in JavaScript?",
+        multiChoice: ["returns length of time", "returns length of string", "returns length of flexbox", "returns length of css"],
+        answer: "returns length of string"
+    },
+
+    {
+        question: "What operator returns the remainder left over when one operand is divided by a second operand.",
+        multiChoice: ["+=", "&", "%", "#"],
+        answer: "% Modulo"
+    },
+
+    {
+        question: "What operator returns the remainder left over when one operand is divided by a second operand.",
+        multiChoice: ["+=", "&", "%", "#"],
+        answer: "% Modulo"
+    },
+
+];
+
+answers[]
---
+++ script.js
@@ -0,0 +1,27 @@
+// var for minutes
+const startingMinutes = 1;
+// let for seconds because want to change 
+let time = startingMinutes * 60;
+
+const countdownEL = document.getElementById('countdown');
+
+setInterval(updateCountdown, 1000);
+
+
+function updateCountdown(){
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countdownEL.innerHTML = `${minutes}:${seconds}`;
+
+    time--;
+
+    if(time === 0){
+        clearInterval(updateCountdown);
+    }
+    
+}
+
+
