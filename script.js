670ec41e0bc6e3ade07cbb8b046dea3bcad8995b
james <james.anthony.russell36@gmail.com>
2022-09-05 18:16:22 +1000

    changed the questions so  can be called by id

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// for that, we have created an array of objects, 
+// in which each object has a question and corresponding options and 
+// the information of the correct answer. 
+// With this array of objects, we have the JSON format so it helps us to deal 
+// with the API and to deal with the real data which is of JSON type most of the time.
+
 // var for minutes
 const startingMinutes = 1;
 // let for seconds because want to change 
@@ -5,9 +11,15 @@ let time = startingMinutes * 60;
 
 const countdownEL = document.getElementById('countdown');
 
+
+
+
+
+
+
+
+
 t = setInterval(updateCountdown, 1000);
-
-
 
 function updateCountdown(){
     const minutes = Math.floor(time / 60);
